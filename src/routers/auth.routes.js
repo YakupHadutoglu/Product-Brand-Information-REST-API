5303cc34ada3f2feb310240c7863a449efb8a2a5
YakupHadutoglu <yakuphad2004@gmail.com>
2025-05-02 19:00:26 +0300

    feat: Added Google OAuth strategy and token-based authentication with JWT
    - Implemented Google OAuth authentication using Passport.js
    - Generated JWT token upon successful authentication
    - Stored token in a cookie for session management
    - Updated callback route to set token cookie for user session

--- src/routers/auth.routes.js
+++ src/routers/auth.routes.js
@@ -1,13 +1,33 @@
 const userController = require('../controllers/auth.controller.js');
 const express = require('express');
 const router = express.Router();
-const {createUser , loginUser , logOutUser } = userController;
-const { verifyToken } = require('../middlewares/verify.js');
+const passport = require('passport');
+
+const { createUser, loginUser, logOutUser } = userController;
+require('../middlewares/verify.js');
 
 router.route('/register').post(createUser);
 router.route('/login').post(loginUser);
 router.route('/logout').get(logOutUser);
 
+router.get('/google',
+    passport.authenticate('google', { scope: ['profile', 'email'] }));
+
+
+
+router.get('/google/callback',
+    passport.authenticate('google', { failureRedirect: '/' }),
+    (req, res) => {
+        // Google OAuth başarılı olduktan sonra
+        const token = req.user.token;
+
+        // Cookie'ye token'ı kaydediyoruz
+        res.cookie('token', token, { httpOnly: true, secure: false }); // secure: true olmalı prod ortamında
+
+        // Kullanıcıyı anasayfaya yönlendirebiliriz
+        res.redirect('/');
+    }
+);
 
 module.exports = router;
 
