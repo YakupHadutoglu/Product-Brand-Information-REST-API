74f4118bb73c68cc9e2e432faed65104a844a747
YakupHadutoglu <yakuphad2004@gmail.com>
2025-07-12 17:10:16 +0300

    feat(api-contribution): User API contribution module implemented
    - Users can now submit their own developed or suggested API services as contributions
    - API information is stored using a new Mongoose model

--- src/routers/contribution.routes.js
+++ src/routers/contribution.routes.js
@@ -1,11 +1,12 @@
 const express = require('express');
 const router = express.Router();
-const { brandProduct , newIdea } = require('../controllers/contribution.controller.js');
+const { brandProduct , newIdea , api } = require('../controllers/contribution.controller.js');
 const { authenticateUser, authenticateApprovedStatus } = require('../middlewares/authenticate.js');
 const { noCache } = require('../middlewares/noCache.js');
 
 // Contribution routes
 router.route('/contribution-brand-product').post(authenticateUser, authenticateApprovedStatus, noCache, brandProduct); 4
 router.route('/contribution-new-idea').post(authenticateUser, authenticateApprovedStatus, noCache, newIdea);
+router.route('/contribution-api-service').post(authenticateUser, authenticateApprovedStatus, noCache, api);
 
 module.exports = router;
