434b75db913fb0e68cc6039f9a70f1359f13ba5c
yakup <yakuphad200@gmail.com>
2025-07-12 19:38:56 +0300

    feat(admin-dashboard): contributions are now visible to admin users
    - Admin dashboard updated to display brand, idea, API, and mail contributions
    - Due to unrelated errors triggered by Node.js internal TS files, some code temporarily commented out
    - System confirmed working and tested before error occurrence; temporary workaround applied

---
+++ src/services/contributionAGet.service.js
@@ -0,0 +1,22 @@
+
+// const BrandProduct = require('../models/BrandProduct.js');
+// const Api = require('../models/ContributionApi.js');
+// const Mail = require('../models/Mail.js');
+// const NewIdea = require('../models/Newİdea.js');
+
+
+// const contributionGet = async () => {
+//     try {
+//         const brand = await BrandProduct.find();
+//         const idea = await NewIdea.find();
+//         const api = await Api.find();
+//         const mail = await Mail.find();
+
+//         return { brand, idea, api, mail };
+//     } catch (error) {
+//         console.error('Error getting contributions:', error);
+//         throw new Error('Error while fetching contribution data', { cause: error });
+//     }
+// };
+
+// module.exports = { contributionGet };
